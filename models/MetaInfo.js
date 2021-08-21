d709455de8c8641825bf747e854a30e6bfb5ca70
mugilan18 <mugilanlearny@gmail.com>
2021-08-21 14:07:16 +0530

    space

---
+++ models/MetaInfo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// create Schema
+const metainfoSchema = new Schema({
+    id: {
+    type: Schema.Types.ObjectId,
+    ref: "contents",
+  },
+
+  ProcedureName: {
+    type: String,
+  },
+
+  labtype: {
+    type: String,
+  },
+  
+  department: {
+    type: String,
+  },
+
+  semester: {
+    type: String,
+  },
+
+  year: {
+    type: String,
+  },
+
+  college: {
+    type: String,
+  },
+  
+  status: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = MetaInfo = mongoose.model("metainfo", metainfoSchema);
